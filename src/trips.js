8bef5fb94c6ab6b39210eedba7ea61e3a9b8eecd
CBradsen <117617970+CBradsen@users.noreply.github.com>
2023-04-21 11:57:49 -0600

    feature: Set up trip class and tests

--- src/trips.js
+++ src/trips.js
@@ -1,10 +1,28 @@
-import
+import tripData from '../src/trips-sample-data'
 
-class Trips() {
-  constructor(destinationData, tripsData, userID)
+class Trips {
+  constructor(destinationData, tripData, userID) {
+    this.destinationData = destinationData;
+    this.tripData = tripData;
+    this.userID = userID;
+  }
 
-}
+  getPastTrips(userID) {
 
+  }
 
+  getUpcomingTripsAll() {
+
+  }
+
+  getPendingTrips() {
+
+  }
+
+  caculateTotalSpent() {
+
+  }
+
+};
 
 export default Trips;
