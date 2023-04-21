e10c4302462db6f755ae5236092e0294d2b73cb6
CBradsen <117617970+CBradsen@users.noreply.github.com>
2023-04-21 11:18:04 -0600

    feature-Add all tests and methods for Travelers Class

--- src/travelers.js
+++ src/travelers.js
@@ -1,6 +1,49 @@
-class Travelers {
-  constructor()
+import clients from '../src/client-list';
 
+class Travelers {
+  constructor(clientsData) {
+    this.clients = clientsData
+    this.travelerID = 0 
+    this.travelerFullName = ""
+    this.travelerFirstName = ""
+    this.travelerType = ""
+  }
+
+  findIdWithLogin(username) {
+    if (username.startsWith("traveler", 0, 8)) {
+   const currentID = parseInt(username.slice(8))
+   this.travelerID = currentID
+
+    } else {
+      return "That is not a valid username. Please try again"
+    }
+   return
+  }
+
+  getTravelersFullName() {
+    const person = clients.find(person => person["id"] === this.travelerID)
+    if (person) {
+    this.travelerFullName = person.name
+    } else {
+      console.log("No traveler found")
+    }
+    return this.travelerFullName
+  }
+  getTravelersFirstName() {
+    const splitName = (this.travelerFullName.split(" "))
+    if (splitName) {
+    this.travelerFirstName = splitName[0];
+    }
+    return this.travelerFirstName; 
+  }
+
+  getTravelersType() {
+    const travelerType = clients.find(traveler => this.travelerID)
+    if (travelerType) {
+    this.travelerType = travelerType["travelerType"]
+    }
+    return this.travelerType
+  }
 }
 
 export default Travelers;
---
+++ src/trips.js
@@ -0,0 +1,10 @@
+import
+
+class Trips() {
+  constructor(destinationData, tripsData, userID)
+
+}
+
+
+
+export default Trips;
